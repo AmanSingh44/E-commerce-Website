0a416462c8a2a82824003fe73982f603f2aa4e32
AmanSingh44 <aman7474362@gmail.com>
2023-07-16 15:01:20 +0545

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import Navigation from "./Navigation/Nav";
+import Products from "./Products/Products";
+import Recommendation from "./Recommendation/Recommendation";
+
+function App() {
+    return ( <
+        >
+        <
+        Navigation / >
+        <
+        Products / >
+        <
+        Recommendation / >
+        <
+        />
+    )
+}
+
+export default App;
